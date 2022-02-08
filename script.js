81ecad1e5813e9d2cde5a1f260071fa2147aa480
castellomariaemilia <maria.e.castello@exxonmobil.com>
2022-02-08 11:47:58 -0300

    icone de download

--- script.js
+++ script.js
@@ -14,7 +14,7 @@ $(document).ready(function () {
             var html = '';
             for (let i = 0; i < docs.length; i++) {
                 html += '<tr class="em-c-table__row ">'
-                html += '<td class="em-c-table__cell " colspan="">' + docs[i].FileLeafRef + '<a xmlns:xlink="http://www.w3.org/1999/xlink" xlink:href="unity-1.6.0/images/24/em-icons.svg#download" href="' + docs[i].ServerRedirectedEmbedUrl + '"> d </a>' + '</td>'
+                html += '<td class="em-c-table__cell " colspan="">' + docs[i].FileLeafRef + '<a href="' + docs[i].ServerRedirectedEmbedUrl + '"> <span><svg class="em-c-icon em-c-icon--medium"><use xmlns:xlink="http://www.w3.org/1999/xlink" xlink:href="https://mysite.na.xom.com/personal/sa_jadpcar/UpstreamFileSearch/Shared%20Documents/projeto1/unity-1.6.0/images/24/em-icons.svg#download"></use></svg></span> </a>' + '</td>'
                 html += '<td class="em-c-table__cell " colspan="">' + docs[i].FI_x0020_Doc_x0020_Type + '</td>'
                 html += '<td class="em-c-table__cell " colspan="">' + docs[i].FI_x0020_Instance_x002C__x0020_Tool_x002C__x0020_Technology + '</td>'
                 html += '<td class="em-c-table__cell " colspan="">' + docs[i].FI_x0020_Team_x002C__x0020_Process_x002C__x0020_Script_x0020_Group + '</td>'
