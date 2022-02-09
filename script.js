957d54c4d2eb634705ea5fb0cc13379544739901
JoseExxon <jose.a.cardoso@exxonmobil.com>
2022-02-09 09:21:43 -0300

    listagem dos filtros completa

--- script.js
+++ script.js
@@ -7,14 +7,48 @@ $(document).ready(function () {
         xhrFields: {
             withCredentials: true
         },
-        success: function (data) {
-            console.log(JSON.stringify(data));// need to show success
-            docs = data.Row
-            console.log(docs)
+        success: function (data) {           
+            docs = data.Row           
             var html = '';
+            var htmlFilter1='';
+            var htmlFilter2='';
+            auxfilter = data.Row.map(e=>e.FI_x0020_Instance_x002C__x0020_Tool_x002C__x0020_Technology)
+            auxfilter2 = data.Row.map(e=>e.FI_x0020_Team_x002C__x0020_Process_x002C__x0020_Script_x0020_Group)
+            
+            for(let i = 1; i < auxfilter.length; i++){
+                auxfilter[0] = auxfilter[0].concat(auxfilter[i])
+            }
+            filter = [...new Set(auxfilter[0])]
+            filter2 = [...new Set(auxfilter2)]
+            
+            let cont = 1;
+            for (let i = 0; i < filter.length; i++) {               
+                htmlFilter1 += '<li class="em-c-dropdown-check__item">'
+                htmlFilter1 += '<label class="em-c-input-group em-c-checkbox--container " for="option-' + cont +'">'
+                htmlFilter1 += '<input id="option-' + cont +'" type="checkbox" name="' + filter[i] + '" value="' + filter[i] + '" class="em-c-input-group__control" checked>'
+                htmlFilter1 +=  '<span class="em-c-checkbox--checkmark"></span>'
+                htmlFilter1 +=  '<span class="em-c-input-group__text">' + filter[i] + '</span>'
+                htmlFilter1 += '</label>'
+                htmlFilter1 += '</li>'
+                cont++;
+            }
+            $(".filter1").html(htmlFilter1);
+
+            for (let i = 0; i < filter2.length; i++) {               
+                htmlFilter2 += '<li class="em-c-dropdown-check__item">'
+                htmlFilter2 += '<label class="em-c-input-group em-c-checkbox--container " for="option-' + cont +'">'
+                htmlFilter2 += '<input id="option-' + cont +'" type="checkbox" name="' + filter2[i] + '" value="' + filter2[i] + '" class="em-c-input-group__control" checked>'
+                htmlFilter2 +=  '<span class="em-c-checkbox--checkmark"></span>'
+                htmlFilter2 +=  '<span class="em-c-input-group__text">' + filter2[i] + '</span>'
+                htmlFilter2 += '</label>'
+                htmlFilter2 += '</li>'
+                cont++;
+            }
+            $(".filter2").html(htmlFilter2);
+
             for (let i = 0; i < docs.length; i++) {
                 html += '<tr class="em-c-table__row ">'
-                html += '<td class="em-c-table__cell " colspan="">' + docs[i].FileLeafRef + '<a href="' + docs[i].ServerRedirectedEmbedUrl + '"> <span><svg class="em-c-icon em-c-icon--medium"><use xmlns:xlink="http://www.w3.org/1999/xlink" xlink:href="https://mysite.na.xom.com/personal/sa_jadpcar/UpstreamFileSearch/Shared%20Documents/projeto1/unity-1.6.0/images/24/em-icons.svg#download"></use></svg></span> </a>' + '</td>'
+                html += '<td class="em-c-table__cell " colspan="">' + docs[i].FileLeafRef + '<a style="text-decoration:none" href="' + docs[i].ServerRedirectedEmbedUrl + '"> <span><svg class="em-c-icon em-c-icon--medium"><use xmlns:xlink="http://www.w3.org/1999/xlink" xlink:href="https://mysite.na.xom.com/personal/sa_jadpcar/UpstreamFileSearch/Shared%20Documents/projeto1/unity-1.6.0/images/24/em-icons.svg#download"></use></svg></span> </a>' + '</td>'
                 html += '<td class="em-c-table__cell " colspan="">' + docs[i].FI_x0020_Doc_x0020_Type + '</td>'
                 html += '<td class="em-c-table__cell " colspan="">' + docs[i].FI_x0020_Instance_x002C__x0020_Tool_x002C__x0020_Technology + '</td>'
                 html += '<td class="em-c-table__cell " colspan="">' + docs[i].FI_x0020_Team_x002C__x0020_Process_x002C__x0020_Script_x0020_Group + '</td>'
@@ -22,7 +56,7 @@ $(document).ready(function () {
                 html+= '</tr>'
             }
             $(".docs").html(html);
-            console.log(html);
+            
         }
     });
 });
